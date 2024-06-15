642f03f608654bc9a372de2272d70fbbb56d163a
Zack-O7 <suhail24ck@gmail.com>
2024-06-15 16:31:37 +0530

    Added SignIn API

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -30,10 +30,67 @@ router.post("/signup", (req, res) => {
           return res.status(200).json({
             status: "200", 
             message: "Query Executed",
-            data: [{ statusMessage: "Registration successful", fullName: fullName }]
+            data: [{ statusMessage: "Registration Successful", fullName: fullName }]
           });
         });
         
       });
     });
+
+
+    router.post("/signin", (req, res) => {
+      console.log(req.body);
+      const sql =
+        "SELECT * FROM user_details where email = ?";
+        db.query(sql, [req.body.email], (err, result) => {
+          if (err) {
+            console.error("Error occurred:", err);
+            return res.status(500).json({ message: "Database query error" });
+          }
+          if (result.length === 0) {
+            return res.status(400).json({ message: "User not found" });
+          }
+
+          const user = result[0];
+
+          bcrypt.compare(
+            req.body.password.toString(),
+            user.password,
+            (err, isMatch) => {
+              if (err) {
+                console.error("Error in comparing password:", err);
+                return res
+                  .status(500)
+                  .json({ message: "Error in comparing password" });
+              }
+              if (!isMatch) {
+                return res.status(400).json({ message: "Invalid credentials" });
+              }
+      
+              // const accessToken = jwt.sign(
+              //   { email: req.body.email },
+              //   "yourAccessTokenSecret",
+              //   {
+              //     expiresIn: "1d",
+              //   }
+              // );
+      
+              return res
+                .status(200)
+                // .cookie("auth-token", accessToken, {
+                //   maxAge: 24 * 60 * 60 * 1000,
+                //   httpOnly: true,
+                // })
+                .json({
+                  message: "Login Successful",
+                  firstName: user.firstName,
+                  lastName: user.lastName,
+                  email: user.email
+                });
+            }
+          );
+        });
+      });
+
+
 module.exports = router;
