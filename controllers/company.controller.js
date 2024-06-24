592f2a595483d0700bfaea5e91eeb9bc2a38c2a4
Zack-O7 <suhail24ck@gmail.com>
2024-06-24 17:58:34 +0530

    Added API /addNewCompany

--- controllers/company.controller.js
+++ controllers/company.controller.js
@@ -2,13 +2,19 @@ const db = require("../config/db.js");
 
 const addNewCompany = (req, res) => {
   const { companyName, companyEmail, website } = req.body;
-  const defaultAvatar =
-    "https://raw.githubusercontent.com/nz-m/public-files/main/dp.jpg";
+  // const defaultAvatar =
+  //   "https://raw.githubusercontent.com/nz-m/public-files/main/dp.jpg";
+  // const logo = req?.file?.filename
+  //   ? `${req.protocol}://${req.get("host")}/storage/app/public/${
+  //       req.file.filename
+  //     }`
+  //   : defaultAvatar;
+
   const logo = req?.file?.filename
     ? `${req.protocol}://${req.get("host")}/storage/app/public/${
         req.file.filename
       }`
-    : defaultAvatar;
+    : null;
 
   const sql =
     "INSERT into company_details (companyName, companyEmail, logo, website) VALUES (?,?,?,?)";
