00b3f5cb226c7b1cc3e17e0442cc51cc81de905e
Zack-O7 <suhail24ck@gmail.com>
2024-06-26 15:38:10 +0530

    Added API /addAllCompanies

--- controllers/company.controller.js
+++ controllers/company.controller.js
@@ -1,16 +1,16 @@
 const db = require("../config/db.js");
 
 const addNewCompany = (req, res) => {
-  // const { companyName, companyEmail, website } = req.body;
-  // const defaultAvatar =
-  //   "https://raw.githubusercontent.com/nz-m/public-files/main/dp.jpg";
-  // const logo = req?.file?.filename
-  //   ? `${req.protocol}://${req.get("host")}/storage/app/public/${
-  //       req.file.filename
-  //     }`
-  //   : defaultAvatar;
+  const { companyName, companyEmail, website } = req.body;
+  const defaultAvatar =
+    "https://raw.githubusercontent.com/nz-m/public-files/main/dp.jpg";
+  const logo = req?.file?.filename
+    ? `${req.protocol}://${req.get("host")}/storage/app/public/${
+        req.file.filename
+      }`
+    : defaultAvatar;
 
-    const { companyName, companyEmail, logo, website } = req.body;
+  // const { companyName, companyEmail, logo, website } = req.body;
 
   const sql =
     "INSERT into company_details (companyName, companyEmail, logo, website) VALUES (?,?,?,?)";
@@ -33,19 +33,19 @@ const addNewCompany = (req, res) => {
   });
 };
 
-      //Get
-      const getAllCompanies = (req, res) => {
-        const sql = "SELECT * FROM company_details";
-        db.query(sql, (err, results) => {
-          if(err) {
-            console.error("Error Occurred:", err);
-            return res.status(500).send("Something went wrong");
-          }
-          return res.status(200).json({status: "200", 
-            message: "Query Executed",
-            data: results});
-        });
-      }; 
+//Get
+const getAllCompanies = (req, res) => {
+  const sql = "SELECT * FROM company_details";
+  db.query(sql, (err, results) => {
+    if (err) {
+      console.error("Error Occurred:", err);
+      return res.status(500).send("Something went wrong");
+    }
+    return res
+      .status(200)
+      .json({ status: "200", message: "Query Executed", data: results });
+  });
+};
 
 module.exports = {
   addNewCompany,
