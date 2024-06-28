a79cad01f77eefba205c7dfe60aa7d5321fc89eb
Zack-O7 <suhail24ck@gmail.com>
2024-06-28 21:13:16 +0530

    Updated code

--- controllers/company.controller.js
+++ controllers/company.controller.js
@@ -1,97 +1,62 @@
-// const db = require("../config/db.js");
-
-// const addNewCompany = (req, res) => {
-//   const { companyName, companyEmail, website } = req.body;
-//   const defaultAvatar =
-//     "https://raw.githubusercontent.com/nz-m/public-files/main/dp.jpg";
-//   // const logo = req?.file?.filename
-//   //   ? `${req.protocol}://${req.get("host")}/storage/app/public/${
-//   //       req.file.filename
-//   //     }`
-//   //   : defaultAvatar;
-
-//   const logo = req?.file?.filename
-//     ? `${req.protocol}://${req.get("host")}/storage/app/public/${
-//         req.file.filename
-//       }`
-//     : null;
-
-//   // const { companyName, companyEmail, logo, website } = req.body;
-
-//   const sql =
-//     "INSERT into company_details (companyName, companyEmail, logo, website) VALUES (?,?,?,?)";
-//   db.query(sql, [companyName, companyEmail, logo, website], (err, result) => {
-//     if (err) {
-//       console.error("Error occurred:", err);
-//       return res.status(500).send("Something went wrong");
-//     }
-//     return res.status(200).json({
-//       status: "200",
-//       message: "Query Executed",
-//       data: [
-//         {
-//           statusMessage: "Company Created Successfully",
-//           id: result.insertId,
-//           companyName: companyName,
-//         },
-//       ],
-//     });
-//   });
-// };
-
-// //Get
-// const getAllCompanies = (req, res) => {
-//   const sql = "SELECT * FROM company_details";
-//   db.query(sql, (err, results) => {
-//     if (err) {
-//       console.error("Error Occurred:", err);
-//       return res.status(500).send("Something went wrong");
-//     }
-//     return res
-//       .status(200)
-//       .json({ status: "200", message: "Query Executed", data: results });
-//   });
-// };
-
-// module.exports = {
-//   addNewCompany,
-//   getAllCompanies,
-// };
-
 const db = require("../config/db.js");
 
 const addNewCompany = (req, res) => {
   const { companyName, companyEmail, website } = req.body;
-  const logo = req.file ? req.file.path : null;
+  const defaultAvatar =
+    "https://raw.githubusercontent.com/nz-m/public-files/main/dp.jpg";
+  // const logo = req?.file?.filename
+  //   ? `${req.protocol}://${req.get("host")}/storage/app/public/${
+  //       req.file.filename
+  //     }`
+  //   : defaultAvatar;
+
+  const logo = req.file
+    ? // const logo = req?.file?.filename
+      `${req.protocol}://${req.get("host")}/storage/app/public/${
+        req.file.filename
+      }`
+    : null;
+
+  // const { companyName, companyEmail, logo, website } = req.body;
 
   if (!companyName || !companyEmail || !website || !logo) {
-    return res.status(400).send({ message: "All fields are required, including the logo." });
+    return res
+      .status(400)
+      .send({ message: "All fields are required, including the logo." });
   }
 
-  // Proceed with database insertion
-  const query = "INSERT INTO company_details (companyName, companyEmail, logo, website) VALUES (?, ?, ?, ?)";
-
-  db.query(query, [companyName, companyEmail, logo, website], (err, result) => {
+  const sql =
+    "INSERT into company_details (companyName, companyEmail, logo, website) VALUES (?,?,?,?)";
+  db.query(sql, [companyName, companyEmail, logo, website], (err, result) => {
     if (err) {
-      console.error(err);
-      return res.status(500).send({ message: "Database error" });
+      console.error("Error occurred:", err);
+      return res.status(500).send("Something went wrong");
     }
-
-    res.status(201).send({ message: "Company added successfully", companyId: result.insertId });
+    return res.status(200).json({
+      status: "200",
+      message: "Query Executed",
+      data: [
+        {
+          statusMessage: "Company Created Successfully",
+          id: result.insertId,
+          companyName: companyName,
+        },
+      ],
+    });
   });
 };
 
-
+//Get
 const getAllCompanies = (req, res) => {
-  const query = "SELECT * FROM company_details";
-
-  db.query(query, (err, results) => {
+  const sql = "SELECT * FROM company_details";
+  db.query(sql, (err, results) => {
     if (err) {
-      console.error(err);
-      return res.status(500).send({ message: "Database error" });
+      console.error("Error Occurred:", err);
+      return res.status(500).send("Something went wrong");
     }
-
-    res.status(200).send(results);
+    return res
+      .status(200)
+      .json({ status: "200", message: "Query Executed", data: results });
   });
 };
 
--- server.js
+++ server.js
@@ -1,23 +1,3 @@
-// const express = require("express");
-// const cors = require("cors");
-// const bodyParser = require("body-parser");
-// const userRoutes = require("./routes/auth.routes.js");
-// const companyRoutes = require("./routes/company.routes.js");
-
-// const app = express();
-// app.use(cors());
-// app.use(express.json({ limit: "5mb" }));
-// app.use("/storage", express.static(__dirname + "/storage"));
-// const PORT = 4000;
-
-// app.use(bodyParser.json());
-// app.use("/api/users", userRoutes);
-// app.use("/api/company", companyRoutes);
-
-// app.listen(PORT, () => {
-//   console.log(`Server is running on port ${PORT}`);
-// });
-
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
