280c748912962aca1e9746fba6adfca739c110b8
Zack-O7 <suhail24ck@gmail.com>
2024-07-18 00:01:18 +0530

    Employee API

---
+++ routes/employee.routes.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+const employeeController = require("../controllers/employee.controller.js");
+
+router.post("/addNewEmployee", employeeController.addNewEmployee);
+router.get("/getAllEmployees", employeeController.getAllEmployees);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const userRoutes = require("./routes/auth.routes.js");
 const companyRoutes = require("./routes/company.routes.js");
+const employeeRoutes = require("./routes/employee.routes.js");
 
 const app = express();
 const PORT = 4000;
@@ -23,6 +24,7 @@ app.use("/storage", express.static(__dirname + "/storage"));
 app.use(bodyParser.json());
 app.use("/api/users", userRoutes);
 app.use("/api/company", companyRoutes);
+app.use("/api/employee", employeeRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
