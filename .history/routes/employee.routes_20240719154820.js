9f830672188d9b33476061cbf8f8cbbf18d690fb
Zack-O7 <suhail24ck@gmail.com>
2024-07-19 16:46:07 +0530

    Company Update API

--- .history/routes/employee.routes_20240719154820.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const employeeController = require("../controllers/employee.controller.js");
-
-
-router.post("/addNewEmployee", employeeController.addNewEmployee);
-router.get("/getAllEmployees", employeeController.getAllEmployees);
-
-module.exports = router;
